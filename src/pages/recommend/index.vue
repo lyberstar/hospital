4c9b86fe6aaa22556ff40ba06375cffb8a05252a
weijiaN <450837248@qq.com>
2020-06-14 22:11:43 +0800

    修复高度

--- src/pages/recommend/index.vue
+++ src/pages/recommend/index.vue
@@ -368,7 +368,11 @@ export default {
     }
     .box-contain{
       width: 100%;
-      padding: 12px;
+      height:auto!important;
+      height: 63px;
+      min-height:63px;
+      padding-left: 12px;
+      padding-top: 12px;
       box-sizing: border-box;
       background:rgba(240,255,239,1);
       border-radius:8px;
@@ -376,7 +380,6 @@ export default {
       position: relative;
       overflow: hidden;
       margin-bottom: 12px;
-      height: 63px;
       &:last-child{
         margin-bottom: 78px!important;
       }
